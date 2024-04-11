7f7da799fac404c9b8fce12728e3a5ff4e4b4a9d
Vladislav Radovic <vradovic522@gmail.com>
2024-04-11 20:20:26 +0200

    Initial commit

---
+++ src/main/java/com/vradovic/cloudspring/CloudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vradovic.cloudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vradovic/cloudspring/CloudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vradovic.cloudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
