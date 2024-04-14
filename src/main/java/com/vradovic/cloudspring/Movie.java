486a9f248cab6c63ba90e6c916ad55db99baac2e
Vladislav Radovic <vradovic522@gmail.com>
2024-04-14 16:04:50 +0200

    Add custom table name

--- src/main/java/com/vradovic/cloudspring/Movie.java
+++ src/main/java/com/vradovic/cloudspring/Movie.java
@@ -2,13 +2,11 @@ package com.vradovic.cloudspring;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "${TABLE_NAME}")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
